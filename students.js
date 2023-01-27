90d2869031c5a930038630f72db87931a272c5cd
erdembn <72571747+erdembn@users.noreply.github.com>
2023-01-27 23:38:35 +0300

    Initial commit

---
+++ students.js
@@ -0,0 +1,26 @@
+let students = [
+    {
+        name: "Mark",
+        age: 23,
+        grades: [75, 88, 75]
+    },
+    {
+        name: "Barbara",
+        age: 22,
+        grades: [80, 100, 70, 96]
+    },
+    {
+        name: "Alex",
+        age: 18,
+        grades: [45, 98, 85]
+    },
+    {
+        name: "John",
+        age: 20,
+        grades: [10, 100, 75, 96]
+    }
+];
+
+
+module.exports = students;
+
